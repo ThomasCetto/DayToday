079ca8d23e28e647fd74e1cd7c9069b062ebf9d1
ThomasCetto <thomas.cetto04@gmail.com>
2026-02-23 20:19:10 +0100

    Added basic insert form for tasks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // prevent reload
+    const formData = new FormData(e.target);
+    const payload = Object.fromEntries(formData.entries());
+    const response = await fetch("/api/tasks/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+    });
+
+    const data = await response.json();
+    console.log("Status:", response.status);
+    console.log("Success:", response.ok);
+    console.log("Response body:", data);
+  };
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn moron
-      </p>
-    </>
+      <h1>Add task</h1>
+      <form method='POST' onSubmit={handleSubmit}>
+        <input name='title' placeholder='Task title'/><br/>
+        <input name='description' placeholder='Task description'/><br/>
+        <input name='date' placeholder='date' type='date'/><br/>
+        <label>
+          <input type='checkbox' value="Yes" />
+          Repeat task
+        </label><br/>
+        
+        <select name='gapType'>
+          <option value='day' selected>Day</option>
+          <option value='week'>Week</option>
+          <option value='month'>Month</option>
+          <option value='year'>Year</option>
+        </select><br/>
+        <input name='gapAmount' placeholder='Every how much'/><br/>
+        <button type='submit'>Add task</button>
+
+
+        {/* <input type='submit'>Add task</input> */}
+      </form>
+  </>
   )
 }
 
